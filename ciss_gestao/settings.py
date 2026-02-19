4a00af620580b42e8059b42510a3cdeeef4a2e0e
luan <luan640@gmail.com>
2026-02-19 08:03:55 -0300

    configurando e-mail

--- ciss_gestao/settings.py
+++ ciss_gestao/settings.py
@@ -274,3 +274,17 @@ TENANCY_EXEMPT_PATH_PREFIXES = [
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/auth/login/'
+
+# Email (SMTP)
+EMAIL_BACKEND = os.getenv(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.smtp.EmailBackend',
+)
+EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = get_bool('EMAIL_USE_TLS', True)
+EMAIL_USE_SSL = get_bool('EMAIL_USE_SSL', False)
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'no-reply@localhost')
+SERVER_EMAIL = os.getenv('SERVER_EMAIL', DEFAULT_FROM_EMAIL)
--- ciss_gestao/urls.py
+++ ciss_gestao/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -106,6 +107,36 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/login/', TenantLoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'auth/password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='auth/password_reset.html',
+            email_template_name='auth/password_reset_email.html',
+            subject_template_name='auth/password_reset_subject.txt',
+        ),
+        name='password_reset',
+    ),
+    path(
+        'auth/password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='auth/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'auth/password-reset/confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='auth/password_reset_confirm.html',
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'auth/password-reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
+    ),
     path('auth/select-company/', CompanySelectView.as_view(), name='company-select'),
     path('master/', MasterDashboardView.as_view(), name='master-dashboard'),
     path('master/metrics/', MasterCompanyMetricsView.as_view(), name='master-metrics'),
